1922fa147d3fd2eaf30f535604d5c0eb1570b56d
Sandra Thieme <thieme@synyx.de>
2018-01-18 16:16:17 +0100

    Allow https:// and ssh:// scm uris

--- github-release-plugin/src/test/kotlin/utils.kt
+++ github-release-plugin/src/test/kotlin/utils.kt
@@ -9,5 +9,11 @@ class TestSCMParse {
         assertEquals(
                 Repo("https://api.github.com", "cy6erGn0m", "github-release-plugin"),
                 "scm:git:git@github.com:cy6erGn0m/github-release-plugin.git".parseSCMUrl())
+        assertEquals(
+                Repo("https://api.github.com", "cy6erGn0m", "github-release-plugin"),
+                "scm:git:https://github.com/cy6erGn0m/github-release-plugin.git".parseSCMUrl())
+        assertEquals(
+                Repo("https://api.github.com", "cy6erGn0m", "github-release-plugin"),
+                "scm:git:ssh://git@github.com/cy6erGn0m/github-release-plugin.git".parseSCMUrl())
     }
 }
